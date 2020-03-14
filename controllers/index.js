3a54af18530a5284a5803d45a4e3033892f166ff
Les Lockett <les.lockett@hotmail.com>
2020-03-14 09:56:15 +0000

    Add user profile

--- controllers/index.js
+++ controllers/index.js
@@ -2,6 +2,7 @@ const User = require('../models/user');
 const Post = require('../models/post');
 const passport = require('passport');
 const mapBoxToken = process.env.MAPBOX_TOKEN;
+const util = require('util');
 
 module.exports = {
 	// GET /
@@ -74,5 +75,26 @@ module.exports = {
 	// GET /logout
 	getLogout (req, res, next) {
 		req.logout(), res.redirect('/');
+	},
+
+	// Get /profile
+	async getProfile (req, res, next) {
+		const posts = await Post.find()
+			.where('author')
+			.equals(req.user._id)
+			.limit(10)
+			.exec();
+		res.render('profile', { posts });
+	},
+	async updateProfile (req, res, next) {
+		const { username, email } = req.body;
+		const { user } = res.locals;
+		if (username) user.username = username;
+		if (email) user.email = email;
+		await user.save();
+		const login = util.promisify(req.login.bind(req));
+		await login(user);
+		req.session.success = 'Your profile has successfully updated';
+		res.redirect('profile');
 	}
 };
--- middleware/index.js
+++ middleware/index.js
@@ -28,5 +28,39 @@ module.exports = {
 		}
 		req.session.error = 'Access denied!';
 		res.redirect('back');
+	},
+	isValidPassword   : async (req, res, next) => {
+		const { user } = await User.authenticate()(
+			req.user.username,
+			req.body.currentPassword
+		);
+		if (user) {
+			// add user to res.locals
+			res.locals.user = user;
+			next();
+		} else {
+			req.session = 'Incorrec current password';
+			return res.redirect('/profile');
+		}
+	},
+	changePassword    : async (req, res, next) => {
+		const { newPassword, passwordConfirmation } = req.body;
+
+		if (newPassword && !passwordConfirmation) {
+			req.session.error = 'Missing password confirmation';
+			return res.redirect('/profile');
+		}
+		if (newPassword && passwordConfirmation) {
+			const { user } = res.locals;
+			if (newPassword === passwordConfirmation) {
+				await user.setPassword(newPassword);
+				next();
+			} else {
+				req.session.error = 'New password must match!';
+				return res.redirect('/profile');
+			}
+		} else {
+			next();
+		}
 	}
 };
